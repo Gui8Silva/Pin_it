59c3427c4b90dfc02d86af6b54c7a9437e168663
Gui8Silva <guilhermea@ipvc.pt>
2021-05-05 20:10:42 +0100

    Maps + Menu Updates

--- app/src/main/java/com/example/pin_it/Menu.kt
+++ app/src/main/java/com/example/pin_it/Menu.kt
@@ -1,19 +1,34 @@
 package com.example.pin_it
 
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 
 class Menu : AppCompatActivity() {
+
+    private lateinit var shared_preferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
     }
 
-    //fun mapaActivity(view: View){
-    //    val intent = Intent(this@Menu, MapsActivity::class.java)
-    //    startActivity(intent)
-    //}
+    fun MapsActivity(view: View){
+        val intent = Intent(this@Menu, MapsActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun logout(view: View){
+        val shared_preferences_edit : SharedPreferences.Editor = shared_preferences.edit()
+        shared_preferences_edit.clear()
+        shared_preferences_edit.apply()
+
+        val intent = Intent(this@Menu, Login::class.java)
+        startActivity(intent)
+        finish()
+    }
 }
